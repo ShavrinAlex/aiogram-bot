c2fbffd45677c833df2b6951618debdff8f412b2
Alex Shavrin <alex.shavrin200303mail.ru>
2021-07-16 22:55:56 +0300

    Научился работать с состояниями, появилась подсказка команд, отлавливает неизвестные комманды

--- handlers/messages/__init__.py
+++ handlers/messages/__init__.py
@@ -1,4 +1,5 @@
 from .commands import dp
 from .calculator import dp
+from .unknow_command import dp
 
 __all__ = ['dp']
---
+++ handlers/messages/unknow_command.py
@@ -0,0 +1,14 @@
+from aiogram.dispatcher import FSMContext
+from aiogram.types import Message
+import keyboards
+from loader import dp
+
+
+@dp.message_handler(state='*')
+async def message_answer(message: Message, state: FSMContext):
+    await message.answer(
+        text='Неизвестная комманда, вы перемещенны на главное меню',
+        reply_markup=keyboards.main.keyboard
+    )
+
+    await state.finish()
